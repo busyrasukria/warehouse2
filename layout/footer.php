e142163b95e81cdfb0a1ac0c5fb2235b79322843
busyrasukria <busyrasukria@gmail.com>
2025-11-26 11:07:45 +0800

    Create footer.php

---
+++ layout/footer.php
@@ -0,0 +1,17 @@
+ <footer class="mt-0 py-0 text-center">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <p class="text-muted mb-2">
+                         <h6 class="text-uppercase fw-bold mb-2">PEPS-JV (KEDAH) SDN.BHD</h6>
+                    <p class="small mb-1">FG & Warehouse Management System v1.0 | For internal use only</p>
+                    </p>
+                     <hr class="footer-divider">
+                    <p class="text-muted small">
+                        © <?= date('Y') ?> PJVK. All rights reserved.
+                    </p>
+
+                </div>
+            </div>
+        </div>
+    </footer>
